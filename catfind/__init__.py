3db188a5ecd14c64467f1b684a30b6eab876bf7d
Jamie Bliss <jamie@ivyleav.es>
2021-12-13 18:00:58 -0500

    pypi guesser

--- catfind/__init__.py
+++ catfind/__init__.py
@@ -1,9 +1,12 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import importlib.resources
 import json
+import random
+import re
 
 import click
 from flask import Flask, redirect, make_response, request, render_template_string
+import httpx
 from pony.flask import Pony
 from pony import orm
 from pony.orm import select
@@ -169,3 +172,103 @@ def index(url):
     proj.last_indexed = now
 
     # TODO: Clean up old entries
+
+
+@app.cli.command('auto-index')
+@orm.db_session
+def auto_index():
+    """
+    Automatically select & index one project.
+
+    Won't index a project more than once a day, and will prefer older projects
+    """
+    now = datetime.now(timezone.utc)
+    index_before = now - timedelta(days=1)
+    projs = select(p for p in projects if not p.last_indexed or p.last_indexed <= index_before)[:]
+    weights = [(now - p.last_indexed).total_seconds() for p in projs]
+
+    if not projs:
+        return
+
+    proj, = random.choices(projs, weights=weights)
+
+    index(proj.inv_url)
+
+
+def resolve(url):
+    try:
+        resp = httpx.head(url, follow_redirects=True)
+    except httpx.ReadTimeout:
+        pass
+    except httpx.ConnectError:
+        pass
+    else:
+        if resp.is_success:
+            return resp.url
+
+
+def guess_url(url):
+    if not url:
+        return
+    # Join then check for redirects
+    url1 = resolve(httpx.URL(url).join('objects.inv'))
+
+    # Check for redirects and then join
+    url2 = resolve(url)
+    if url2:
+        url2 = resolve(url2.join('objects.inv'))
+
+    if url1:
+        yield url1
+
+    if url2:
+        yield url2
+
+
+def check_for_inventory(url):
+    resp = httpx.get(url, follow_redirects=True)
+    if not resp.is_success:
+        return False
+    chunk = next(resp.iter_bytes())
+    return chunk.startswith(b'# Sphinx')
+
+
+# From https://stackoverflow.com/questions/6038061/regular-expression-to-find-urls-within-a-string
+URL_PATTERN = re.compile('(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])', re.I)
+
+
+@app.cli.command('pypi-guess')
+@click.argument("pkg")
+@orm.db_session
+def pypi_guess(pkg):
+    """
+    Given a PyPI package name, guess its object inventory
+    """
+    resp = httpx.get(f"https://pypi.org/pypi/{pkg}/json", follow_redirects=True)
+    resp.raise_for_status()
+    data = resp.json()
+
+    roots = []
+    if data['info']['project_urls']:
+        roots += [u for u in data['info']['project_urls'].values() if resolve(u)]
+
+    if data['info']['docs_url'] and resolve(data['info']['docs_url']):
+        roots.append(data['info']['docs_url'])
+
+    # Find any URLs in the README
+    for m in URL_PATTERN.finditer(data['info']['description']):
+        roots.append(m.group(0))
+
+    urls = {
+        u
+        for urls in map(guess_url, roots)
+        for u in urls
+        if check_for_inventory(u)
+    }
+
+    if urls:
+        print("Found URLs:")
+        for u in urls:
+            print(f"* {u!s}")
+    else:
+        print("No URLs found")
